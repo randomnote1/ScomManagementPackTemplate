608529554857f215bedb29cdad0c5b0a9437d396
Dan Reist <djreist@outlook.com>
2021-05-20 21:12:28 -0400

    Added base and head ref

--- Build/build.ps1
+++ Build/build.ps1
@@ -1,6 +1,9 @@
 # Set the verbose preference
 $VerbosePreference = 'Continue'
 
+Write-Verbose -Message "GITHUB_BASE_REF: $($env:GITHUB_BASE_REF)"
+Write-Verbose -Message "GITHUB_HEAD_REF: $($env:GITHUB_HEAD_REF)"
+
 # Download the System Center Visual Studio Authoring Extensions (VSAE)
 $invokeWebRequestParams = @{
 	Uri = 'https://download.microsoft.com/download/4/4/6/446B60D0-4409-4F94-9433-D83B3746A792/VisualStudioAuthoringConsole_x64.msi'
